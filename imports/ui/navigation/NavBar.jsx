86aab354d9834a26d6a1a207b9ae7ea26a809b9a
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-07-22 15:00:04 -0700

    Modified NavBar to be stateless
    - utilized `.defaultProps` to set `pages` as an empty array if nothing is passed in.
    - moved propType declarations outside.

--- imports/ui/navigation/NavBar.jsx
+++ imports/ui/navigation/NavBar.jsx
@@ -5,45 +5,23 @@ import NavItem from './NavItem';
 
 import { navBar } from '../styles/navigation.css';
 
-export default class NavBar extends React.Component {
-  static propTypes = {
-    pages: arrayOf(
-      shape({
-        route: string.isRequired,
-        title: string.isRequired,
-      }),
-    ),
-  }
+const NavBar = ({ pages }) => (
+  <nav className={navBar}>
+    { pages.map(page => <NavItem key={page.title} {...page} />) }
+  </nav>
+);
 
-  static defaultProps = {
-    pages: [],
-  }
+NavBar.propTypes = {
+  pages: arrayOf(
+    shape({
+      route: string.isRequired,
+      title: string.isRequired,
+    }),
+  ),
+};
 
-  state = {
-    pages: [],
-  }
+NavBar.defaultProps = {
+  pages: [],
+};
 
-  componentDidMount() {
-    const { pages } = this.state;
-
-    if (!pages.length) {
-      this.setPages();
-    }
-  }
-
-  setPages = () => {
-    const { pages } = this.props;
-
-    this.setState({ pages });
-  }
-
-  render() {
-    const { pages } = this.state;
-
-    return (
-      <nav className={navBar}>
-        { pages.map(page => <NavItem key={page.title} {...page} />) }
-      </nav>
-    );
-  }
-}
+export default NavBar;
